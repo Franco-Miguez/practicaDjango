fe5a242520f30735e1ee6ff86a6037923c518d53
Franco Miguez <franco.miguez98@gmail.com>
2022-03-25 15:29:39 -0300

    casi terminando

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,23 +1,36 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import PostCreateForm
+from .models import Post
 
 # Create your views here.
 class BlogViewsList(View):
     def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
         context ={
-
+            'posts' : posts
         }
         return render(request, 'blog_list.html', context)
 
 class BlogCreateView(View):
     def get(self, request, *arg, **kwarg):
+        form = PostCreateForm()
         context = {
-
+            'form': form
         }
         return render(request, 'blog_create.html', context)
     def post(self, request, *arg, **kwarg):
+        if request.method=="POST":
+            form = PostCreateForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get("title")
+                content = form.cleaned_data.get("content")
+
+                p, create = Post.objects.get_or_create(title=title,content=content)
+                p.save()
+                return redirect('blog:home')
+
         context = {
 
         }
